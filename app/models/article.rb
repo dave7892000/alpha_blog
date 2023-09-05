beefbdebd9b31e3dce140b99a599fd4377f67d7d
David Steketee <david.steketee@gmail.com>
2023-09-05 09:41:03 -0400

    Add one-to-many association between users and articles from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 #config.autoload_paths += %W(#{config.root}/models)
 
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 4, maximum: 100}
     validates :description, presence: true, length: { minimum: 4, maximum: 300}
 
